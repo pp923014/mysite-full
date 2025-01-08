e3843b6199106476371b458bcbfbc50594f0178a
PRIYANSHU KUMAR PANDEY <125594596+pp923014@users.noreply.github.com>
2025-01-08 15:38:45 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        'http://localhost:8000/api/v1/user/login',
+        'https://mysite-full-backend.onrender.com/api/v1/user/login',
         formData
       );
 
