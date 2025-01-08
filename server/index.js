d1ca4af57f0974fb962dd9aa1c759d4d5e894fc2
PRIYANSHU KUMAR PANDEY <125594596+pp923014@users.noreply.github.com>
2025-01-08 15:21:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,9 +30,9 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/trainee", traineeRoute);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+   // res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 })
 
 
